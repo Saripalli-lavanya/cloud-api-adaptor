78c2931b17404c5fbd811de951158ef8c490bcd2
Saripalli Lavanya <Saripalli.Lavanya@ibm.com>
2024-07-16 16:17:33 +0530

    Update build-s390x-se-image.sh

--- src/cloud-api-adaptor/podvm/qcow2/build-s390x-se-image.sh
+++ src/cloud-api-adaptor/podvm/qcow2/build-s390x-se-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 set -e  # Exit immediately if any command fails
 
 export LANG=C.UTF-8
@@ -85,6 +85,8 @@ sudo mkfs.ext4 -L "root" /dev/mapper/${LUKS_NAME}
 sudo mkdir -p ${dst_mnt}
 sudo mkdir -p ${src_mnt}
 sudo mount /dev/mapper/$LUKS_NAME ${dst_mnt}
+sudo mount
+ls -lrh ${dst_mnt}
 sudo mkdir ${dst_mnt}/boot-se
 sudo mount -o norecovery ${tmp_nbd}1 ${dst_mnt}/boot-se
 sudo mount --bind -o ro / ${src_mnt}
@@ -116,13 +118,14 @@ sudo chmod 600 "${dst_mnt}/etc/keys/luks-${dev_uuid}.key"
 
 # Add LUKS keyfile to crypttab
 echo "Add LUKS keyfile to crypttab"
-sudo touch ${dst_mnt}/etc/crypttab
+sudo touch "${dst_mnt}"/etc/crypttab
 sudo -E bash -c 'echo "${LUKS_NAME} UUID=$(sudo blkid -s UUID -o value ${tmp_nbd}2) /etc/keys/luks-$(blkid -s UUID -o value /dev/mapper/${LUKS_NAME}).key luks,discard,initramfs" > ${dst_mnt}/etc/crypttab'
 echo "ls ${dst_mnt}/etc/crypttab"
-ls ${dst_mnt}/etc/
-cat ${dst_mnt}/etc/crypttab
+ls -ltrh "${dst_mnt}"/etc/crypttab
+ls "${dst_mnt}"/etc/
+cat "${dst_mnt}"/etc/crypttab
 
-sudo chmod 744 ${dst_mnt}/etc/crypttab
+sudo chmod 744 "${dst_mnt}"/etc/crypttab
 
 # Disable virtio_rng
 sudo -E bash -c 'echo "blacklist virtio_rng" > ${dst_mnt}/etc/modprobe.d/blacklist-virtio.conf'
@@ -131,7 +134,7 @@ sudo -E bash -c 'echo "s390_trng" > ${dst_mnt}/etc/modules'
 # Configure dracut and zipl
 sudo -E bash -c 'echo "install_items+=\" /etc/keys/*.key \"" >> ${dst_mnt}/etc/dracut.conf.d/cryptsetup.conf'
 sudo -E bash -c 'echo "UMASK=0077" >> ${dst_mnt}/etc/dracut.conf.d/initramfs.conf'
-sudo -E bash -c 'cat <<END > ${dst_mnt}/etc/zipl.conf
+sudo -E bash -c 'cat <<END > "${dst_mnt}"/etc/zipl.conf
 [defaultboot]
 default=linux
 target=/boot-se
@@ -144,44 +147,47 @@ targetoffset=2048
 [linux]
 image = /boot-se/se.img
 END'
-
-# Update initramfs and GRUB configuration
-echo "Updating initramfs and GRUB configuration"
+cat "${dst_mnt}"/etc/zipl.conf
+echo "Updating initramfs"
 sudo chroot "${dst_mnt}" dracut -f /boot/initramfs-$(uname -r).img $(uname -r) || true
 echo "ls ${dst_mnt}"
-ls ${dst_mnt}
+ls -ltrh ${dst_mnt}
 echo "ls ${dst_mnt}/boot/"
-ls ${dst_mnt}/boot/
+ls -ltrh ${dst_mnt}/boot/
 echo "ls /boot/"
-ls /boot/
+ls -ltrh /boot/
 
 # Create SE boot image
 echo "Creating IBM Secure Execution boot image"
 KERNEL_FILE=/boot/vmlinuz-$(uname -r)
 INITRD_FILE=${dst_mnt}/boot/initramfs-$(uname -r).img
 export SE_PARMLINE="root=/dev/mapper/${LUKS_NAME} panic=0 blacklist=virtio_rng swiotlb=262144 console=ttyS0 printk.time=0 systemd.getty_auto=0 systemd.firstboot=0 module.sig_enforce=1 quiet loglevel=0 systemd.show_status=0"
-sudo -E bash -c 'echo "${SE_PARMLINE}" > ${dst_mnt}/boot/parmfile'
+sudo touch "${dst_mnt}"/boot/parmfile
+sudo -E bash -c 'echo "${SE_PARMLINE}" > "${dst_mnt}"/boot/parmfile'
 echo "printing param file"
-cat ${dst_mnt}/boot/parmfile
+ls -lrh "${dst_mnt}"/boot/
+cat "${dst_mnt}"/boot/parmfile
 
 sudo /usr/bin/genprotimg \
     --verbose \
-    -i ${KERNEL_FILE} \
-    -r ${INITRD_FILE} \
-    -p ${dst_mnt}/boot/parmfile \
+    -i "${KERNEL_FILE}" \
+    -r "${INITRD_FILE}" \
+    -p "${dst_mnt}"/boot/parmfile \
     --no-verify \
-    ${host_keys} \
-    -o ${dst_mnt}/boot-se/se.img
+    "${host_keys}" \
+    -o "${dst_mnt}"/boot-se/se.img
 
 # Check if SE image was created
 if [ ! -e ${dst_mnt}/boot-se/se.img ]; then
     echo "Failed to create SE image, exiting..."
     exit 1
 fi
+ls -ltrh "${dst_mnt}"/boot-se/se.img
+
 
 # Run zipl to prepare boot partition
 echo "Running zipl to prepare boot partition"
-sudo chroot ${dst_mnt} zipl --targetbase ${tmp_nbd} \
+sudo chroot "${dst_mnt}" zipl --targetbase ${tmp_nbd} \
     --targettype scsi \
     --targetblocksize 512 \
     --targetoffset 2048 \
