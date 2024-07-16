8ca81bee21aa4c3676fba9653b69ab36d2d55676
Saripalli Lavanya <Saripalli.Lavanya@ibm.com>
2024-07-16 07:08:43 +0530

    Update build-s390x-se-image.sh

--- src/cloud-api-adaptor/podvm/qcow2/build-s390x-se-image.sh
+++ src/cloud-api-adaptor/podvm/qcow2/build-s390x-se-image.sh
@@ -120,8 +120,12 @@ END'
 # Update initramfs and GRUB configuration
 echo "Updating initramfs and GRUB configuration"
 sudo chroot "${dst_mnt}" dracut -f /boot/initramfs-$(uname -r).img $(uname -r) || true
+
+echo "cat /boot/grub2/grub.cfg"
 cat /boot/grub2/grub.cfg
-sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+
+sudo grub2-mkconfig -v -o /boot/grub2/grub.cfg
+echo "cat /boot/grub2/grub.cfg after grub update"
 cat /boot/grub2/grub.cfg
 
 # Create SE boot image
@@ -131,6 +135,7 @@ INITRD_FILE=/boot/initramfs-$(uname -r).img
 export SE_PARMLINE="root=/dev/mapper/${LUKS_NAME} panic=0 blacklist=virtio_rng swiotlb=262144 console=ttyS0 printk.time=0 systemd.getty_auto=0 systemd.firstboot=0 module.sig_enforce=1 quiet loglevel=0 systemd.show_status=0"
 sudo -E bash -c 'echo "${SE_PARMLINE}" > ${dst_mnt}/boot/parmfile'
 sudo /usr/bin/genprotimg \
+    --verbose \
     -i ${KERNEL_FILE} \
     -r ${INITRD_FILE} \
     -p ${dst_mnt}/boot/parmfile \
