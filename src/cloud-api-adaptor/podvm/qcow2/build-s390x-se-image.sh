69a764d755e2e50f8f3f2f64d59d6e937726b8c4
Saripalli Lavanya <Saripalli.Lavanya@ibm.com>
2024-07-16 22:42:05 +0530

    Update build-s390x-se-image.sh

--- src/cloud-api-adaptor/podvm/qcow2/build-s390x-se-image.sh
+++ src/cloud-api-adaptor/podvm/qcow2/build-s390x-se-image.sh
@@ -12,6 +12,7 @@ elif [ "${ARCH}" != "s390x" ]; then
     echo "Building of SE podvm image is only supported for s390x, exiting..."
     exit 1
 fi
+
 echo "Finding host key files"
 host_keys=""
 rm /tmp/files/.dummy.crt || true
@@ -20,17 +21,14 @@ for i in /tmp/files/*.crt; do
     echo "found host key file: \"${i}\""
     host_keys+="${i} "
 done
+
 [[ -z $host_keys ]] && echo "Didn't find host key files, please download host key files to 'files' folder " && exit 1
+
 cp $host_keys /root/HKD.crt
 chmod 554 /root/HKD.crt
 echo "Building SE podvm image for $ARCH"
 
 # Ensure jq is installed
-# echo "Checking for jq..."
-# if ! command -v jq > /dev/null; then
-#     echo "jq is not installed. Installing..."
-#     sudo dnf install -y jq
-# fi
 echo "jq version:"
 jq --version
 
@@ -41,8 +39,6 @@ device=$(sudo lsblk --json | jq -r --arg disksize "$disksize" '.blockdevices[] |
 tmp_nbd="/dev/$device"
 dst_mnt=$workdir/dst_mnt
 src_mnt=$workdir/src_mnt
-ls -ltrh /
-ls -ltrh /etc/
 echo "Found target device: $device"
 
 # Set up disk and partitions
@@ -52,16 +48,11 @@ sudo parted -a optimal ${tmp_nbd} mklabel gpt \
     mkpart root 256MiB 6400MiB \
     mkpart data 6400MiB ${disksize} \
     set 1 boot on
-ls -ltrh /
-ls -ltrh /etc/
+
 # Wait for partitions to be created
 echo "Waiting for partitions to be detected..."
 while ! sudo ls ${tmp_nbd}1 || ! sudo ls ${tmp_nbd}2; do
     sleep 1
-done
-while true; do
-sleep 1
-[ -e ${tmp_nbd}2 ] && break
 done
 
 # Format boot-se partition
@@ -69,78 +60,33 @@ echo "Formatting boot-se partition"
 sudo mke2fs -t ext4 -L boot-se ${tmp_nbd}1
 boot_uuid=$(sudo blkid ${tmp_nbd}1 -s PARTUUID -o value)
 export boot_uuid
-# Set up encrypted root partition
-echo "Setting up encrypted root partition"
-sudo mkdir -p ${workdir}/rootkeys
-sudo mount -t tmpfs rootkeys ${workdir}/rootkeys
-sudo dd if=/dev/random of=${workdir}/rootkeys/rootkey.bin bs=1 count=64
-# echo YES | sudo cryptsetup luksFormat --type luks2 ${tmp_nbd}2 --key-file ${workdir}/rootkeys/rootkey.bin
-LUKS_NAME="luks-$(sudo blkid -s UUID -o value ${tmp_nbd}2)"
-export LUKS_NAME
-echo "luks name is: $LUKS_NAME"
-# sudo cryptsetup open ${tmp_nbd}2 $LUKS_NAME --key-file ${workdir}/rootkeys/rootkey.bin
 
 # Copy the root filesystem
-echo "Copying root filesystem to encrypted partition"
-# sudo mkfs.ext4 -L "root" ${tmp_nbd}2
+echo "Copying root filesystem to partition"
 sudo mke2fs -t ext4 -L root ${tmp_nbd}2
 boot_uuid2=$(sudo blkid ${tmp_nbd}2 -s PARTUUID -o value)
 export boot_uuid2
 sudo mkdir -p ${dst_mnt}
 sudo mkdir -p ${src_mnt}
-# sudo mount /dev/mapper/$LUKS_NAME /home/peerpod/dst_mnt
-sudo mount
-sudo lsblk
-ls -lrh ${dst_mnt}
-sudo mkdir ${dst_mnt}/boot-se
-sudo mount -o norecovery ${tmp_nbd}1 /home/peerpod/dst_mnt/boot-se
 sudo mount --bind -o ro / ${src_mnt}
 sudo tar --numeric-owner --preserve-permissions --acl --xattrs --xattrs-include='*' --sparse --one-file-system -cf - -C ${src_mnt} . | sudo tar -xf - -C ${dst_mnt}
 sudo umount ${src_mnt}
 echo "Partition copy complete"
-echo "Preparing secure execution boot image"
-sudo rm -rf ${dst_mnt}/home/peerpod/*
 
-sudo mount -t sysfs sysfs /home/peerpod/dst_mnt/sys
-sudo mount -t proc proc /home/peerpod/dst_mnt/proc
-sudo mount --bind /dev /home/peerpod/dst_mnt/dev
-sudo mkdir -p /home/peerpod/dst_mnt/etc/keys
-sudo mount -t tmpfs keys /home/peerpod/dst_mnt/etc/keys
-
-echo "Adding fstab"
-echo "Configuring filesystems and boot setup"
-sudo -E bash -c 'cat <<END > /home/peerpod/dst_mnt/etc/fstab
+# Add entries to fstab
+echo "Adding fstab entries"
+sudo -E bash -c 'cat <<END > /etc/fstab
 #This file was auto-generated
 PARTUUID=${boot_uuid2}    /        ext4  defaults 1 1
-PARTUUID=${boot_uuid}    /boot-se    ext4  norecovery 1 2
+PARTUUID=${boot_uuid}    /boot-se    ext4  defaults 1 2
 END'
-sudo chmod 644 /home/peerpod/dst_mnt/etc/fstab
-cat /home/peerpod/dst_mnt/etc/fstab
-
-# echo "Adding luks keyfile for fs"
-# dev_uuid=$(sudo blkid -s UUID -o value "/dev/mapper/$LUKS_NAME")
-# sudo cp "${workdir}/rootkeys/rootkey.bin" "/home/peerpod/dst_mnt/etc/keys/luks-${dev_uuid}.key"
-# sudo chmod 600 "/home/peerpod/dst_mnt/etc/keys/luks-${dev_uuid}.key"
-
-# Add LUKS keyfile to crypttab
-# echo "Add LUKS keyfile to crypttab"
-# sudo touch /home/peerpod/dst_mnt/etc/crypttab
-# sudo -E bash -c 'echo "${LUKS_NAME} UUID=$(sudo blkid -s UUID -o value ${tmp_nbd}2) /etc/keys/luks-$(blkid -s UUID -o value /dev/mapper/${LUKS_NAME}).key luks,discard,initramfs" > /home/peerpod/dst_mnt/etc/crypttab'
-# echo "ls ${dst_mnt}/etc/crypttab"
-# ls -ltrh "${dst_mnt}"/etc/crypttab
-# ls "${dst_mnt}"/etc/
-# cat "${dst_mnt}"/etc/crypttab
-
-# sudo chmod 744 /home/peerpod/dst_mnt/etc/crypttab
 
 # Disable virtio_rng
-sudo -E bash -c 'echo "blacklist virtio_rng" > /home/peerpod/dst_mnt/etc/modprobe.d/blacklist-virtio.conf'
-sudo -E bash -c 'echo "s390_trng" > /home/peerpod/dst_mnt/etc/modules'
+sudo -E bash -c 'echo "blacklist virtio_rng" > /etc/modprobe.d/blacklist-virtio.conf'
+sudo -E bash -c 'echo "s390_trng" > /etc/modules'
 
-# Configure dracut and zipl
-# sudo -E bash -c 'echo "install_items+=\" /etc/keys/*.key \"" >> /home/peerpod/dst_mnt/etc/dracut.conf.d/cryptsetup.conf'
-sudo -E bash -c 'echo "UMASK=0077" >> /home/peerpod/dst_mnt/etc/dracut.conf.d/initramfs.conf'
-sudo -E bash -c 'cat <<END > /home/peerpod/dst_mnt/etc/zipl.conf
+# Configure zipl
+sudo -E bash -c 'cat <<END > /etc/zipl.conf
 [defaultboot]
 default=linux
 target=/boot-se
@@ -152,75 +98,38 @@ targetoffset=2048
 [linux]
 image = /boot-se/se.img
 END'
-cat "${dst_mnt}"/etc/zipl.conf
+
+# Update initramfs
 echo "Updating initramfs"
-sudo chroot "${dst_mnt}" dracut -f /boot/initramfs-$(uname -r).img $(uname -r) || true
-echo "ls ${dst_mnt}"
-ls -ltrh ${dst_mnt}
-echo "ls ${dst_mnt}/boot/"
-ls -ltrh ${dst_mnt}/boot/
-echo "ls /boot/"
-ls -ltrh /boot/
+sudo dracut -f /boot/initramfs-$(uname -r).img $(uname -r) || true
 
 # Create SE boot image
 echo "Creating IBM Secure Execution boot image"
 KERNEL_FILE=/boot/vmlinuz-$(uname -r)
-INITRD_FILE=${dst_mnt}/boot/initramfs-$(uname -r).img
+INITRD_FILE=/boot/initramfs-$(uname -r).img
 export SE_PARMLINE="root=/ panic=0 blacklist=virtio_rng swiotlb=262144 console=ttyS0 printk.time=0 systemd.getty_auto=0 systemd.firstboot=0 module.sig_enforce=1 quiet loglevel=0 systemd.show_status=0"
-sudo touch /home/peerpod/dst_mnt/boot/parmfile
-sudo -E bash -c 'echo "root=/ panic=0 blacklist=virtio_rng swiotlb=262144 console=ttyS0 printk.time=0 systemd.getty_auto=0 systemd.firstboot=0 module.sig_enforce=1 quiet loglevel=0 systemd.show_status=0" > /home/peerpod/dst_mnt/boot/parmfile'
-echo "printing param file"
-ls -lrh "${dst_mnt}"/boot/
-cat "${dst_mnt}"/boot/parmfile
-lsblk
 sudo /usr/bin/genprotimg \
     --verbose \
     -i "${KERNEL_FILE}" \
     -r "${INITRD_FILE}" \
-    -p "${dst_mnt}"/boot/parmfile \
+    -p "/boot/parmfile" \
     --no-verify \
     -k "/root/HKD.crt" \
-    -o "${dst_mnt}"/boot-se/se.img
+    -o "/boot-se/se.img"
 
 # Check if SE image was created
-if [ ! -e ${dst_mnt}/boot-se/se.img ]; then
+if [ ! -e /boot-se/se.img ]; then
     echo "Failed to create SE image, exiting..."
     exit 1
 fi
-ls -ltrh "${dst_mnt}"/boot-se/se.img
-ls -ltrh "${dst_mnt}"/boot-se
 
 # Run zipl to prepare boot partition
 echo "Running zipl to prepare boot partition"
-sudo chroot "${dst_mnt}" zipl --targetbase ${tmp_nbd} \
+sudo zipl --targetbase ${tmp_nbd} \
     --targettype scsi \
     --targetblocksize 512 \
     --targetoffset 2048 \
     --target /boot-se \
     --image /boot-se/se.img
-lsblk
-echo "ls /root/"
-ls /root/
-echo "ls /etc/"
-ls /etc/
-echo "cat /sys/firmware/uv/prot_virt_guest"
-cat /sys/firmware/uv/prot_virt_guest
-# Clean up
-echo "Cleaning up"
-sudo umount ${workdir}/rootkeys/
-sudo rm -rf ${workdir}/rootkeys
-sudo umount ${dst_mnt}/etc/keys
-sudo umount ${dst_mnt}/boot-se
-sudo umount ${dst_mnt}/dev
-sudo umount ${dst_mnt}/proc
-sudo umount ${dst_mnt}/sys
-# sudo umount ${dst_mnt}
-sudo rm -rf ${src_mnt} ${dst_mnt}
-ls
-cd /
-ls
 
 echo "Script completed successfully"
-echo "Closing encrypted root partition"
-# sudo cryptsetup close $LUKS_NAME
-sleep 10
