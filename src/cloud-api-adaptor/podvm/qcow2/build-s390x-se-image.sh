a7de0e5f15a8c8881893aa05e8449b0ce5faaa22
Saripalli Lavanya <Saripalli.Lavanya@ibm.com>
2024-07-19 21:53:22 +0530

    test-luks

--- src/cloud-api-adaptor/podvm/qcow2/build-s390x-se-image.sh
+++ src/cloud-api-adaptor/podvm/qcow2/build-s390x-se-image.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+export LANG=C.UTF-8
+
+echo "starting************"
+# Check if SE_BOOT variable is set to 1 and architecture is s390x
 if [ "${SE_BOOT:-0}" != "1" ]; then
+    echo "exit**************"
     exit 0
 elif [ "${ARCH}" != "s390x" ]; then
     echo "Building of SE podvm image is only supported for s390x"
     exit 0
 fi
+
 echo "Building SE podvm image for $ARCH"
+
+# Find host key files
 echo "Finding host key files"
 host_keys=""
 rm /tmp/files/.dummy.crt || true
@@ -16,141 +24,196 @@ for i in /tmp/files/*.crt; do
     host_keys+="-k ${i} "
 done
 [[ -z $host_keys ]] && echo "Didn't find host key files, please download host key files to 'files' folder " && exit 1
-if [ "${DISTRO}" = "rhel" ]; then
-    export LANG=C.UTF-8
-    if ! command -v jq &> /dev/null || ! command -v cryptsetup &> /dev/null; then
-        if ! command -v jq &> /dev/null; then
-            echo >&2 "jq is required but it's not installed. Installing now..."
-            sudo yum install jq -y >/dev/null 2>&1
-            if [ $? -ne 0 ]; then
-                echo >&2 "Failed to install jq. Aborting."
-                exit 1
-            fi
-        fi
 
-        if ! command -v cryptsetup &> /dev/null; then
-            echo >&2 "cryptsetup is required but it's not installed. Installing now..."
-            sudo yum install cryptsetup -y >/dev/null 2>&1
-            if [ $? -ne 0 ]; then
-                echo >&2 "Failed to install cryptsetup. Aborting."
-                exit 1
-            fi
-        fi
-    fi
-    echo "jq and cryptsetup are installed. Proceeding with the script..."
-else
-    echo "Installing jq"
-    export DEBIAN_FRONTEND=noninteractive
-    sudo apt-get update > /dev/null 2>&1
-    sudo apt-get install jq -y > /dev/null 2>&1
-    sudo apt-get remove unattended-upgrades -y
-    sudo apt-get autoremove
-    sudo apt-get clean
-fi
-sudo rm -rf /var/lib/apt/lists/*
+# Install necessary packages
+echo "Installing necessary packages"
+#sudo dnf install -y epel-release
+echo "df -h"
+df -h
+#echo "codeready builder"
+#sudo yum repolist
+#subscription-manager repos --enable rhel-9-for-s390x-appstream-rpms
+#sudo yum repolist
+echo "clean all"
+#sudo yum autoremove -y
+sudo yum clean all
+#sudo rm -rf /var/cache/yum
+#echo "eple"
+#sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+echo "jq1"
+#sudo dnf install -y jq
+#sudo dnf install -y https://rpmfind.net/linux/centos-stream/9-stream/AppStream/s390x/os/Packages/jq-1.6-16.el9.s390x.rpm
+jq --version
+#wget http://mirror.centos.org/centos/9-stream/BaseOS/s390x/os/Packages/oniguruma-6.9.6-1.el9.5.s390x.rpm
+#sudo rpm -ivh oniguruma-6.9.6-1.el9.5.s390x.rpm
 
+# Set up disk and partitions
 workdir=$(pwd)
 disksize=100G
 device=$(sudo lsblk --json | jq -r --arg disksize "$disksize" '.blockdevices[] | select(.size == $disksize and .children == null and .mountpoint == null) | .name')
+sudo lsblk --json
+echo $device
 echo "Found target device $device"
-# /dev/vda or /dev/vdb
 export tmp_nbd="/dev/$device"
 export dst_mnt=$workdir/dst_mnt
 export src_mnt=$workdir/src_mnt
-echo "Creating boot-se and root partitions"
 
+echo "Creating boot-se and root partitions"
 sudo parted -a optimal ${tmp_nbd} mklabel gpt \
     mkpart boot-se ext4 1MiB 256MiB \
-    mkpart root 256MiB 6400MiB \
-    mkpart data 6400MiB ${disksize} \
+    mkpart root 256MiB ${disksize} \
     set 1 boot on
+
+df -h 
 
 echo "Waiting for the two partitions to show up"
 while true; do
-sleep 1
-[ -e ${tmp_nbd}2 ] && break
+    sleep 1
+    [ -e ${tmp_nbd}2 ] && break
 done
 
+# Format boot-se partition
 echo "Formatting boot-se partition"
 sudo mke2fs -t ext4 -L boot-se ${tmp_nbd}1
 boot_uuid=$(sudo blkid ${tmp_nbd}1 -s PARTUUID -o value)
 export boot_uuid
+echo " *********uuid $boot_uuid"
 
+# Set up encrypted root partition
 echo "Setting up encrypted root partition"
 sudo mkdir ${workdir}/rootkeys
 sudo mount -t tmpfs rootkeys ${workdir}/rootkeys
 sudo dd if=/dev/random of=${workdir}/rootkeys/rootkey.bin bs=1 count=64 &> /dev/null
+# cat ${workdir}/rootkeys/rootkey.bin
+#    cryptsetup luksFormat \
+#               --type luks2 --key-size 512 --sector-size 4096 --iter-time 2000  \
+#               --cipher aes-xts-plain64 ${tmp_nbd}2 --key-file ${workdir}/rootkeys/rootkey.bin
 echo YES | sudo cryptsetup luksFormat --type luks2 ${tmp_nbd}2 --key-file ${workdir}/rootkeys/rootkey.bin
+echo "${workdir}/rootkeys/*****************"
+ls ${workdir}/rootkeys/
+# cat ${workdir}/rootkeys/rootkey.bin
+sudo mke2fs -t ext4 -L root "${tmp_nbd}"2
 echo "Setting luks name for root partition"
-LUKS_NAME="luks-$(sudo blkid -s UUID -o value ${tmp_nbd}2)"
+LUKS_NAME="$(blkid "${tmp_nbd}"2 -s PARTUUID -o value)"
 export LUKS_NAME
 echo "luks name is: $LUKS_NAME"
 sudo cryptsetup open ${tmp_nbd}2 $LUKS_NAME --key-file ${workdir}/rootkeys/rootkey.bin
-
-echo "Copying the root filesystem"
-sudo mkfs.ext4 -L "root" /dev/mapper/${LUKS_NAME}
+echo "ls root***************"
+ls /root
 sudo mkdir -p ${dst_mnt}
 sudo mkdir -p ${src_mnt}
-sudo mount /dev/mapper/$LUKS_NAME ${dst_mnt}
+sudo mount "${tmp_nbd}"2 ${dst_mnt}
+echo "dstmnt*************"
+ls ${dst_mnt}
 sudo mkdir ${dst_mnt}/boot-se
 sudo mount -o norecovery ${tmp_nbd}1 ${dst_mnt}/boot-se
+echo "********************ls boot-se"
+ls ${dst_mnt}/boot-se
+echo "ls boot"
+ls /boot
 sudo mount --bind -o ro / ${src_mnt}
+echo "********************ls ${src_mnt} srcmnt"
+ls ${src_mnt}
 tar_opts=(--numeric-owner --preserve-permissions --acl --selinux --xattrs --xattrs-include='*' --sparse  --one-file-system)
 sudo tar -cf - "${tar_opts[@]}" --sort=none -C ${src_mnt} . | sudo tar -xf - "${tar_opts[@]}" --preserve-order  -C "$dst_mnt"
+#tar -czf root.tar.gz "${tar_opts[@]}" --sort=none -C "$src_mnt" .
+ls
+echo "********************ls src and dst"
+ls -ltr ${src_mnt}
+echo "************** ls src boot"
+ls -ltr ${src_mnt}/boot
+echo "************** ls dst"
+ls -ltr $dst_mnt
+echo "************** ls dst boot"
+ls -ltr $dst_mnt/boot
+
 sudo umount ${src_mnt}
 echo "Partition copy complete"
 
+# Prepare secure execution boot image
 echo "Preparing secure execution boot image"
+ls ${dst_mnt}/home/peerpod/
 sudo rm -rf ${dst_mnt}/home/peerpod/*
-
+echo "cat new script *********"
 sudo mount -t sysfs sysfs ${dst_mnt}/sys
 sudo mount -t proc proc ${dst_mnt}/proc
 sudo mount --bind /dev ${dst_mnt}/dev
+echo "ls sys"
+ls ${dst_mnt}/sys
+echo "ls proc"
+ls ${dst_mnt}/proc
+echo "cat proc/cmdline"
+cat ${dst_mnt}/proc/cmdline
+echo "ls dev"
+ls ${dst_mnt}/dev
 
 sudo mkdir -p ${dst_mnt}/etc/keys
 sudo mount -t tmpfs keys ${dst_mnt}/etc/keys
-# ADD CONFIGURATION
+echo " ls keys"
+ls ${dst_mnt}/etc/keys
+
+# Add configuration files
 echo "Adding fstab"
 sudo -E bash -c 'cat <<END > ${dst_mnt}/etc/fstab
 #This file was auto-generated
-/dev/mapper/$LUKS_NAME    /        ext4  defaults 1 1
+PARTUUID=$LUKS_NAME    /        ext4  defaults 1 1
 PARTUUID=$boot_uuid    /boot-se    ext4  norecovery 1 2
 END'
-
+echo $boot_uuid 
+cat ${dst_mnt}/etc/fstab
+echo "Adding lsblk"
+lsblk
+echo "Adding blkid"
+blkid
 echo "Adding luks keyfile for fs"
 dev_uuid=$(sudo blkid -s UUID -o value "/dev/mapper/$LUKS_NAME")
+echo "devuuid: $dev_uuid"
 sudo cp "${workdir}/rootkeys/rootkey.bin" "${dst_mnt}/etc/keys/luks-${dev_uuid}.key"
 sudo chmod 600 "${dst_mnt}/etc/keys/luks-${dev_uuid}.key"
+echo "dstmnt keys ************ ${dst_mnt}/etc/keys/luks-${dev_uuid}.key"
+ls ${dst_mnt}/etc/keys/
+cat ${dst_mnt}/etc/keys/luks-${dev_uuid}.key
+echo "key verify"
+cat ${workdir}/rootkeys/rootkey.bin
+ls ${dst_mnt}/etc/keys/
 
 sudo -E bash -c 'cat <<END > ${dst_mnt}/etc/crypttab
 #This file was auto-generated
-$LUKS_NAME UUID=$(sudo blkid -s UUID -o value ${tmp_nbd}2) /etc/keys/luks-$(blkid -s UUID -o value /dev/mapper/$LUKS_NAME).key luks,discard,initramfs
-END'
+#$LUKS_NAME UUID=$(sudo blkid -s UUID -o value ${tmp_nbd}2) /etc/keys/luks-$(blkid -s UUID -o value /dev/mapper/$LUKS_NAME).key luks,discard,initramfs
+#END'
 sudo chmod 744 "${dst_mnt}/etc/crypttab"
+echo "crypttab*************"
+cat ${dst_mnt}/etc/crypttab
+echo "dst etc ********"
+ls ${dst_mnt}/etc/
 
 # Disable virtio_rng
 sudo -E bash -c 'cat <<END > ${dst_mnt}/etc/modprobe.d/blacklist-virtio.conf
 #do not trust rng from hypervisor
 blacklist virtio_rng
 END'
+echo "ls ${dst_mnt}/etc/"
+ls ${dst_mnt}/etc/
+echo "ls ${dst_mnt}/etc/modprobe.d/"
+ls ${dst_mnt}/etc/modprobe.d/
 
 sudo -E bash -c 'echo s390_trng >> ${dst_mnt}/etc/modules'
 
 echo "Preparing files needed for mkinitrd"
-
-if [ "${DISTRO}" = "rhel" ]; then
-    sudo -E bash -c 'echo "install_items+=\"/etc/keys/*.key\"" >> ${dst_mnt}/etc/dracut.conf.d/cryptsetup.conf'
-    sudo -E bash -c 'echo "UMASK=0077" >> ${dst_mnt}/etc/dracut.conf.d/initramfs.conf'
-else 
-    sudo -E bash -c 'echo "KEYFILE_PATTERN=\"/etc/keys/*.key\"" >> ${dst_mnt}/etc/cryptsetup-initramfs/conf-hook'
-    sudo -E bash -c 'echo "UMASK=0077" >> ${dst_mnt}/etc/initramfs-tools/initramfs.conf'
-fi
-    sudo -E bash -c 'cat <<END > ${dst_mnt}/etc/zipl.conf
+ls ${dst_mnt}/etc/
+sudo -E bash -c 'echo "KEYFILE_PATTERN=\"/etc/keys/*.key\"" >> ${dst_mnt}/etc/dracut.conf.d/cryptsetup.conf'
+sudo -E bash -c 'echo "install_items+=\" /etc/keys/*.key \"" >> ${dst_mnt}/etc/dracut.conf.d/cryptsetup.conf'
+sudo -E bash -c 'echo "UMASK=0077" >> ${dst_mnt}/etc/dracut.conf.d/initramfs.conf'
+#sudo -E bash -c 'echo "add_dracutmodules+=\" crypt \"" >> ${dst_mnt}/etc/dracut.conf.d/crypt.conf'
+#sudo -E bash -c 'echo "add_dracutmodules+=\" crypt \"" >> ${dst_mnt}/etc/dracut.conf.d/crypt.conf'
+#sudo -E bash -c 'echo "KEYFILE_PATTERN=\"/etc/keys/*.key\"" >> ${dst_mnt}/etc/cryptsetup-initramfs/conf-hook'
+#sudo -E bash -c 'echo "UMASK=0077" >> ${dst_mnt}/etc/initramfs-tools/initramfs.conf'
+sudo -E bash -c 'cat <<END > ${dst_mnt}/etc/zipl.conf
 [defaultboot]
 default=linux
 target=/boot-se
 
-targetbase=/dev/vda
+targetbase=${tmp_nbd}
 targettype=scsi
 targetblocksize=512
 targetoffset=2048
@@ -159,33 +222,76 @@ targetoffset=2048
 image = /boot-se/se.img
 END'
 
+echo "************** normal /etc"
+ls /etc
+cat /etc/zipl.conf
+echo "************** dst mnt /etc"
+ls ${dst_mnt}/etc/
+cat ${dst_mnt}/etc/zipl.conf
+
 echo "Updating initial ram disk"
-if [ "${DISTRO}" = "rhel" ]; then
-    sudo chroot "${dst_mnt}" dracut -f /boot/initramfs-$(uname -r).img $(uname -r) || true
-else
-    sudo chroot "${dst_mnt}" update-initramfs -u || true
-fi
-echo "!!! Bootloader install errors prior to this line are intentional !!!!!" 1>&2
+echo "before dracut"
+ls ${dst_mnt}/boot/
+
+#sudo chroot ${dst_mnt} dracut -f --regenerate-all
+
+ls /boot/
+ls ${dst_mnt}/boot
+#cp /boot/initramfs-$(uname -r).img ${dst_mnt}/boot/
+echo "dracut initramfs start"
+ls ${dst_mnt}
 echo "Generating an IBM Secure Execution image"
 
 # Clean up kernel names and make sure they are where we expect them
-KERNEL_FILE=$(readlink ${dst_mnt}/boot/vmlinuz)
-INITRD_FILE=$(readlink ${dst_mnt}/boot/initrd.img)
+echo "ls bootdst after dracut"
+ls ${dst_mnt}/boot/
+cp /boot/vmlinuz-$(uname -r) ${dst_mnt}/boot/
+echo "ls bootdst after dracut and vmlinuz"
+ls ${dst_mnt}/boot/
+sudo chroot ${dst_mnt} dracut -f --kver
+ls ${dst_mnt}/boot/
+sudo chroot ${dst_mnt} dracut -f --regenerate-all
+KERNEL_FILE=${dst_mnt}/boot/vmlinuz-$(uname -r)
+INITRD_FILE=${dst_mnt}/boot/initramfs-$(uname -r).img
 echo "Creating SE boot image"
-export SE_PARMLINE="root=/dev/mapper/$LUKS_NAME console=ttysclp0 quiet panic=0 rd.shell=0 blacklist=virtio_rng swiotlb=262144"
+export SE_PARMLINE="root=PARTUUID=$LUKS_NAME console=ttysclp0 quiet panic=0 rd.shell=0 blacklist=virtio_rng swiotlb=262144"
+#export SE_PARMLINE="panic=0 blacklist=virtio_rng swiotlb=262144 cloud-init=disabled console=ttyS0 printk.time=0 systemd.getty_auto=0 systemd.firstboot=0 module.sig_enforce=1 quiet loglevel=0 systemd.show_status=0"
+#export SE_PARMLINE="root=/dev/mapper/$LUKS_NAME console=ttysclp0 quiet panic=0 rd.shell=0 blacklist=virtio_rng swiotlb=262144"
 sudo -E bash -c 'echo "${SE_PARMLINE}" > ${dst_mnt}/boot/parmfile'
+echo "cat parmfile"
+cat ${dst_mnt}/boot/parmfile
+ls ${dst_mnt}/boot
+cat "${host_keys}"
+echo "ls dst"
+ls ${dst_mnt}
+echo "ls dst/etc"
+ls ${dst_mnt}/etc
+echo "keys"
+ls ${dst_mnt}/etc/keys
+echo "ls dst/run"
+ls ${dst_mnt}/run
+echo "ls dst/proc"
+ls ${dst_mnt}/proc
+echo "dst/dev"
+ls ${dst_mnt}/dev
+echo "dst/sys"
+ls ${dst_mnt}/sys
 sudo -E /usr/bin/genprotimg \
-    -i ${dst_mnt}/boot/${KERNEL_FILE} \
-    -r ${dst_mnt}/boot/${INITRD_FILE} \
+    -i ${KERNEL_FILE} \
+    -r ${INITRD_FILE} \
     -p ${dst_mnt}/boot/parmfile \
     --no-verify \
     ${host_keys} \
     -o ${dst_mnt}/boot-se/se.img
-
-# exit and throw an error if no se image was created
+echo "done"
+# Check if SE image was created
 [ ! -e ${dst_mnt}/boot-se/se.img ] && exit 1
-# if building the image succeeded wipe /boot
+echo "not here"
+#cp root.tar.gz ${dst_mnt}/boot-se/
+# Clean /boot directory
 sudo rm -rf ${dst_mnt}/boot/*
+echo "*************ls bootse"
+ls ${dst_mnt}/boot-se
 echo "Running zipl to prepare boot partition"
 sudo chroot ${dst_mnt} zipl --targetbase ${tmp_nbd} \
     --targettype scsi \
@@ -194,7 +300,8 @@ sudo chroot ${dst_mnt} zipl --targetbase ${tmp_nbd} \
     --target /boot-se \
     --image /boot-se/se.img
 
-echo "Cleaning luks keyfile"
+# Clean up
+echo "Cleaning up"
 sudo umount ${workdir}/rootkeys/ || true
 sudo rm -rf ${workdir}/rootkeys
 sudo umount ${dst_mnt}/etc/keys
@@ -207,4 +314,7 @@ sudo rm -rf ${src_mnt} ${dst_mnt}
 
 echo "Closing encrypted root partition"
 sudo cryptsetup close $LUKS_NAME
+
 sleep 10
+
+echo "RHEL-based SE podvm qcow2 image build completed successfully"
